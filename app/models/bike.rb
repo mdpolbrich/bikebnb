ff5b27c418b750d1cb364f1f52858a1a7c6e1a40
philipp kern <philippk8@gmail.com>
2022-07-25 12:48:48 +0200

    model validation and dependent destroy function

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,8 @@
 class Bike < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :bookings
+
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true
+  validates :price, presence: true
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :bike
+
+  validates :start, presence: true
+  validates :end, presence: true
 end
