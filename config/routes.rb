6472626c465484710063244148a7311f85978256
Maria Olbrich <olbrich15@gmail.com>
2022-07-27 05:13:20 +0200

    validations added for bike model

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :bikes
-  # get "bikes/index", to: "bikes#index"
-  # get "bikes/new", to: "bikes#new", as: :new_bike
-  # post "bikes/", to: "bikes#create"
-  # get "bikes/:id/edit", to: "bikes#edit", as: :edit_bike
-  # get "bikes/:id", to: "bikes#show", as: :bike
-  # patch "bikes/:id", to: "bikes#update"
-  # delete "bikes/:id", to: "bikes#destroy"
 end
