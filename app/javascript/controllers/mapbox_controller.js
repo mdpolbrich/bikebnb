d7978c2c0c2fe8dfa57563065743249b44f22338
Monica Jara <jara.monica@gmail.com>
2022-07-30 15:42:55 +0200

    finished basic geocoding

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+// import "mapbox-gl/dist/mapbox-gl.css"
+// import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css"
 
 export default class extends Controller {
   static values = {
@@ -14,5 +17,24 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+
   }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(this.map)
+    });
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
 }
