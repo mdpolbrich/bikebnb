2466ea3ce90b182c50afa19b619f2c4c2344e42e
Maria Olbrich <olbrich15@gmail.com>
2022-08-06 13:50:22 +0200

    final booking

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bikes
-  has_many :bookings
+  has_many :bikes, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
